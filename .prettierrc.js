02b2165dccf0955aef1db27215ff51664be04fe6
kamalradha <radha.k@techigai.io>
2021-12-19 14:01:07 +0530

    test2

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,7 +1,6 @@
 module.exports = {
-  bracketSpacing: false,
-  jsxBracketSameLine: true,
-  singleQuote: false,
-  trailingComma: 'all',
-  arrowParens: 'avoid',
+  "printWidth": 100,
+  "semi": false,
+  "singleQuote": false,
+  "trailingComma": "all"
 };
--- App.tsx
+++ App.tsx
@@ -1,8 +1,8 @@
-import React from "react";
-import {View, Text} from "react-native";
+import React from "react"
+import { View, Text } from "react-native"
 
 const App = () => {
-  return <View />;
-};
+  return <View />
+}
 
-export default App;
+export default App
